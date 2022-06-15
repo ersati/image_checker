8fcca342410d982dd3fa5d85a8d0c561d937e0be
Peter <piotrp92@gmail.com>
2022-06-15 22:24:52 +0100

    final version

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config({ silent: process.env.NODE_ENV === 'dev' });
 import express from 'express';
 import path from 'path';
+import {fileURLToPath} from 'url';
 import got from 'got';
 import multer  from 'multer';
 import fs from 'fs';
 import FormData from 'form-data';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// import 'dotenv/config'
 const app = express();
 
 const storage = multer.diskStorage({
@@ -19,6 +26,7 @@ const upload = multer({ storage: storage });
 
 app.use("/public", express.static('./public/'));
 app.use(express.static("public"))
+app.use(express.static('uploads'));
 app.set('view engine', 'ejs');
 
 const main = (req, res) => {
@@ -26,28 +34,46 @@ const main = (req, res) => {
 }
 
 const photo = (req, res) => {
-    const contex = {
-        url: '/uploads/1655217284041.jpg'
-    }
-    res.render('photo', contex)
+    const img = req.params.id
+    const apiKey = "";
+    const apiSecret = "";
+    const filePath = path.join(__dirname, '/uploads', img);
+    const formData = new FormData();
+    // console.log(apiKey,apiSecret,formData)
+    formData.append('image', fs.createReadStream(filePath));
+    (async () => {
+        try {
+            const response = await got.post('https://api.imagga.com/v2/tags', {body: formData, username: apiKey, password: apiSecret});
+            const obj = JSON.parse(response.body)
+            const status = obj.status.type
+            const tags = obj.result.tags.slice(0,5)
+            console.log(obj.status);
+            const contex = {
+                status,
+                tags 
+            }
+            res.render('photo', contex)
+        } catch (error) {
+            console.log(error);
+        }
+    })();
+
 }
 
 app.get('/', main)
 
-app.get('/photo', photo)
+app.get('/photo/:id', photo)
 app.post('/upload',upload.single('image'),function(req,res){
     const filePath = '/path/to/image.jpg';
     const formData = new FormData();
     formData.append('image', fs.createReadStream(filePath));
-    // console.log(formData)
-    console.log(req.file.path);
+    const imgName = req.file.filename
+    console.log(req.file.filename);
     const contex = {
         url: '/uploads/1655217284041.jpg'
     }
-    res.redirect('/photo');
-
+    res.redirect(`/photo/${imgName}`);
 });
-
 
 let port = process.env.PORT;
 if (port == null || port == "") {
