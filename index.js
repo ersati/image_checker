563b77a1c045f044fdfa206780b6d7b136060539
Peter <piotrp92@gmail.com>
2022-07-15 12:17:31 +0100

    delete unwanted files

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-import path from 'path';
-import {fileURLToPath} from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express';
+import path from 'path';
+import {fileURLToPath} from 'url';
 import got from 'got';
 import multer  from 'multer';
 import fs from 'fs';
 import FormData from 'form-data';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// import 'dotenv/config'
 const app = express();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/')
+    cb(null, 'public/img')
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname)) //Appending extension
@@ -23,16 +25,25 @@ const upload = multer({ storage: storage });
 
 app.use("/public", express.static('./public/'));
 app.use(express.static("public"))
-app.use(express.static('uploads'));
 app.set('view engine', 'ejs');
+
 const main = (req, res) => {
+    fs.readdir('public/img/', (err, files) => {
+        if (err) console.log(err);
+        for (const file of files) {
+            fs.unlink(path.join('public/img/', file), err => {
+                if (err) console.log(err);
+            });
+        }
+    });
     res.render('index')
 }
+
 const photo = (req, res) => {
     const img = req.params.id
-    const apiKey = process.env.APIKey;
+    const apiKey =process.env.APIKey;
     const apiSecret = process.env.APISecret;
-    const filePath = path.join(__dirname, '/uploads', img);
+    const filePath = path.join(__dirname, '/public/img', img);
     const formData = new FormData();
     formData.append('image', fs.createReadStream(filePath));
     (async () => {
@@ -41,23 +52,33 @@ const photo = (req, res) => {
             const obj = JSON.parse(response.body)
             const status = obj.status.type
             const tags = obj.result.tags.slice(0,5)
-            console.log(obj.status);
             const contex = {
                 status,
-                tags 
+                tags,
+                img 
             }
             res.render('photo', contex)
         } catch (error) {
             console.log(error);
         }
     })();
+
 }
 
 app.get('/', main)
+
 app.get('/photo/:id', photo)
 app.post('/upload',upload.single('image'),function(req,res){
-    const imgName = req.file.filename
-    res.redirect(`/photo/--{imgName}`);
+    const filePath = '/path/to/image.jpg';
+    const formData = new FormData();
+    formData.append('image', fs.createReadStream(filePath));
+    if(req.file){
+        const imgName = req.file.filename
+        res.redirect(`/photo/${imgName}`);
+    }else{
+        res.redirect(`/`);
+    }
+    
 });
 
 let port = process.env.PORT;
